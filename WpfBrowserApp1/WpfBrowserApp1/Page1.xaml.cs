385e623736792c9723f329f19edc7bc0c973ecf4
chenh <350951061@qq.com>
2021-02-18 18:04:05 +0800

    增加拖拽桌面图标

--- WpfBrowserApp1/WpfBrowserApp1/Page1.xaml.cs
+++ WpfBrowserApp1/WpfBrowserApp1/Page1.xaml.cs
@@ -23,12 +23,125 @@ namespace WpfBrowserApp1
         public Page1()
         {
             InitializeComponent();
+            //this.button5.MouseLeftButtonDown += canvas_MouseLeftButtonDown;
+            //this.button5.MouseMove += canvas_MouseMove;
+            //this.button5.MouseLeftButtonUp += canvas_MouseLeftButtonUp;
+
+            //this.button5.PreviewMouseLeftButtonDown += canvas_MouseLeftButtonDown;
+
+
+            button5.AddHandler(Button.MouseLeftButtonDownEvent, new MouseButtonEventHandler(canvas_MouseLeftButtonDown), true);//注册事件
+            button5.AddHandler(Button.MouseLeftButtonUpEvent, new MouseButtonEventHandler(canvas_MouseLeftButtonUp), true);//注册事件
+            button5.AddHandler(Button.MouseMoveEvent, new MouseEventHandler(canvas_MouseMove), true);//注册事件   
+
+            //this.button6.MouseLeftButtonDown += canvas_MouseLeftButtonDown;
+            //this.button6.MouseMove += canvas_MouseMove;
+            //this.button6.MouseLeftButtonUp += canvas_MouseLeftButtonUp;
         }
 
 
         private void button1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             MessageBox.Show("double click button1");
+        }
+
+        private void button2_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Point p = e.GetPosition(this.canvas);
+            MessageBox.Show($"{p.X},{p.Y}");
+        }
+
+
+        Point pos = new Point();
+        private void canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Button tmp = (Button)sender;
+            pos = e.GetPosition(this.canvas);
+            tmp.CaptureMouse();
+            tmp.Cursor = Cursors.Hand;
+        }
+
+        private void canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Button tmp = (Button)sender;
+
+            double dx = e.GetPosition(this.canvas).X;
+            double dy = e.GetPosition(this.canvas).Y;
+
+            int fx = (int)((dx - 10) / 100);
+            if (fx > this.WindowWidth - 100)
+            {
+                fx = (int)((this.WindowWidth - 10) / 100 - 1);
+            }
+            if (fx < 0)
+            {
+                fx = 0;
+            }
+            int fy = (int)(dy - 10) / 120;
+            if (fy > this.WindowHeight - 120)
+            {
+                fy = (int)((this.WindowHeight - 10) / 120 - 1);
+            }
+            if (fy <= 0)
+            {
+                fy = 0;
+            }
+
+            dx = fx * 100 + 20;
+            dy = fy * 120 + 20;
+
+            while (true)
+            {
+                bool isFind = false;
+                UIElementCollection children = this.canvas.Children;
+                foreach (UIElement child in children)
+                {
+                    Control c = (Control)child;
+                    if (c.Margin.Top == dy && c.Margin.Left == dx)
+                    {
+                        isFind = true;
+
+                        dy = dy + 120;
+                        if (dy > this.WindowHeight - 120)
+                        {
+                            dy = 20;
+                            dx = dx + 100;
+                        }
+
+                        break;
+                    }
+                }
+
+                if (!isFind)
+                {
+                    break;
+                }
+            }
+
+
+
+
+            tmp.Margin = new Thickness(dx, dy, 0, 0);
+            tmp.ReleaseMouseCapture();
+        }
+
+        /// <summary>
+        /// 一个图标宽80高100，间距20
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Button tmp = (Button)sender;
+                double dx = e.GetPosition(this.canvas).X;
+                double dy = e.GetPosition(this.canvas).Y;
+
+
+                tmp.Margin = new Thickness(dx, dy, 0, 0);
+                pos = e.GetPosition(this.canvas);
+            }
         }
     }
 }
