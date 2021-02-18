805b9b4c81ea7f1cd75727aad60acf221eddbf82
chenh <350951061@qq.com>
2021-02-18 16:26:20 +0800

    自定义桌面图标

---
+++ WpfBrowserApp1/WpfCustomControlLibrary1/DesktopIcon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfCustomControlLibrary1
+{
+    public class DesktopIcon : Button
+    {
+        static DesktopIcon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DesktopIcon), new FrameworkPropertyMetadata(typeof(DesktopIcon)));
+        }
+
+
+    }
+}
---
+++ WpfBrowserApp1/WpfCustomControlLibrary1/IconButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfCustomControlLibrary1
+{
+    public class IconButton : Button
+    {
+        static IconButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(IconButton), new FrameworkPropertyMetadata(typeof(IconButton)));
+        }
+    }
+}
