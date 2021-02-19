b3b9b7be0f59b9c0c9ea82557252e0d2515eb63e
chenh <350951061@qq.com>
2021-02-19 17:30:45 +0800

    带图标的提交按钮

---
+++ WpfBrowserApp1/WpfCustomControlLibrary1/IconSubmit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfCustomControlLibrary1
+{
+    public class IconSubmit : Button
+    {
+        static IconSubmit()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(IconSubmit), new FrameworkPropertyMetadata(typeof(IconSubmit)));
+        }
+
+        public override void EndInit()
+        {
+            base.EndInit();
+
+            this.Background = Brushes.LightGray;
+        }
+
+        /// <summary>
+        /// 图标 不是自带的 必须用RelativeSource绑定 {Binding RelativeSource={RelativeSource Mode=FindAncestor, AncestorType={x:Type local:IconButton}},Path=Icon}
+        /// </summary>
+        public string Icon { get; set; }
+
+
+
+    }
+}
