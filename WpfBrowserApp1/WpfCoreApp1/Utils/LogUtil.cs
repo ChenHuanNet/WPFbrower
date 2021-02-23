0c8310d894eff0c581a951a062ffd05a078a73e3
chenh <350951061@qq.com>
2021-02-23 17:59:34 +0800

    增加NLog日志配置

--- WpfBrowserApp1/WpfCoreApp1/Utils/LogUtil.cs
+++ WpfBrowserApp1/WpfCoreApp1/Utils/LogUtil.cs
@@ -5,6 +5,14 @@ using System.Text;
 
 namespace WpfCoreApp1.Utils
 {
+    /// <summary>
+    /// 安装NUGET包
+    /// NLog.Web.AspNetCore 
+    /// 不需要安装 NLog.Config 包
+    /// 手动加入NLog.config配置文件
+    /// 生成操作-内容
+    /// 输出要复制
+    /// </summary>
     public class LogUtil
     {
         private static readonly Logger log = LogManager.GetLogger("");
